88dda6c30879653a6c0e0f7fbe0a4c3d882e435f
RVBelyaevsky <rooooooma73@gmail.com>
2024-03-17 21:45:51 +0800

    create main and config

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+root_dir = os.path.dirname(__file__)
+products_path = os.path.join(root_dir, 'products.json')
---
+++ main.py
@@ -0,0 +1,43 @@
+import json
+from config import products_path
+
+with open(products_path, encoding='utf-8') as file:
+    data = file.read()
+test_data = json.loads(data)
+
+
+class Category:
+    name: str
+    description: str
+    products: list
+
+    count_categories = 0
+    unique_products = 0
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+        self.count_categories = 1
+
+        Category.count_categories += 1
+        Category.unique_products = len(self.products)
+
+
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+# category_list = []
+# for category in test_data:
+#     category_list.append(Category(category['name'], category['description'], category['products']))
+# print(Category.count_categories)
+# print(category_list)
