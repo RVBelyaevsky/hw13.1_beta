c9c95d314e91342e46e4a1835f14bfef14139e55
RVBelyaevsky <rooooooma73@gmail.com>
2024-03-19 22:33:57 +0800

    do something, omg

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import json
-from config import products_path
-
-with open(products_path, encoding='utf-8') as file:
-    data = file.read()
-test_data = json.loads(data)
+# import json
+# from config import products_path
+#
+# with open(products_path, encoding='utf-8') as file:
+#     data = file.read()
+# test_data = json.loads(data)
 
 
 class Category:
@@ -13,14 +13,26 @@ class Category:
 
     count_categories = 0
     unique_products = 0
-    def __init__(self, name, description, products):
+    def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
         self.__products = products
         self.count_categories = 1
 
         Category.count_categories += 1
-        Category.unique_products = len(self.products)
+        Category.unique_products += len(self.__products)
+
+    @property
+    def products(self):
+        products_str = []
+        for product in self.__products:
+            products_str.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
+        return products_str
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.unique_products += 1
+
 
 
 class Product:
@@ -28,16 +40,68 @@ class Product:
     description: str
     price: float
     quantity: int
+    instances = []
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+        self.instances.append(self)
+
+    @classmethod
+    def create_product(cls, new_product: dict):
+        instance = cls(**new_product)
+        return instance
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print('цена введена некорректная')
+        else:
+            self.__price = new_price
 
 
-# category_list = []
-# for category in test_data:
-#     category_list.append(Category(category['name'], category['description'], category['products']))
+
+
+# product_1 = Product('nokia', 'good telephone', 5000.0, 100)
+# product_2 = Product('sony', 'no good telephone', 2000.0, 10)
+# product_1.price = 0
+# print(product_1.price)
+#
+# products_list = [product_1, product_2]
+# print(products_list)
+#
+# category_1 = Category('telephones', 'old telephones', products_list)
+# print(category_1)
+#
+# print(category_1.products)
+# # for category in test_data:
+# #     category_list.append(Category(category['name'], category['description'], category['products']))
 # print(Category.count_categories)
-# print(category_list)
+# print(Category.unique_products)
+# # print(category_list[0].name, "---", category_list[0].description)
+# # print(category_list[1].name, "---", category_list[1].description)
+# # print(category_list[0])
+# #
+# #
+# # new_product = Product('a', 'b', 150.0, 20)
+# #
+# # category_list[0].add_product(new_product)
+# # print(Category.count_categories)
+# # print(Category.unique_products)
+# # print(category_list[1])
+#
+# new_product = {
+#         "name": "Iphone 15",
+#         "description": "512GB, Gray space",
+#         "price": 210000.0,
+#         "quantity": 8
+#       }
+# print(product_1.instances)
+# product_1.create_product(new_product)
+# print(product_1.instances)
