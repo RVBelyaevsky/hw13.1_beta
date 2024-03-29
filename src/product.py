275bd2cf846ed727949bfbc27b8106d24400d1b3
RVBelyaevsky <rooooooma73@gmail.com>
2024-03-29 09:41:02 +0800

    структурирование папок проекта

---
+++ src/product.py
@@ -0,0 +1,41 @@
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+    instances = []
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+        self.instances.append(self)
+
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})"
+
+
+    def __str__(self):
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        return self.price * self.quantity + other.price * other.quantity
+
+
+    @classmethod
+    def create_product(cls, new_product: dict):
+        instance = cls(**new_product)
+        return instance
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print('цена введена некорректная')
+        else:
+            self.__price = new_price
